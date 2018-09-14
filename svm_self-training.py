09cbd828c137a8821988160b963a5d32c10da786
LuJianHao <39611448+luke10481@users.noreply.github.com>
2018-09-14 15:03:08 +0800

    Update svm_self-training.py

--- svm_self-training.py
+++ svm_self-training.py
@@ -19,15 +19,13 @@ def svm_train():
     # 标签
     label = np.array([int(item[item.rindex(',') + 1:]) for item in text])
 
-    #x_train, x_test, y_train, y_test = train_test_split(content, label, test_size=0, random_state=33)
-    # y_test=np.array(y_test)
     splitline = 3000
 
     U = np.array([i for i in range(0,len(label))])
     L = U[:splitline]
     U = U[splitline:]
     y_train = label[:3000]
-    #SVM = SVC(kernel='rbf', gamma=1, C=44, probability=True)
+
     #SVM = MultinomialNB()
     SVM = SVC(kernel='rbf', gamma=1, C=44, probability=True)
     vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, token_pattern=r'(?u)\b\w+\b')
@@ -38,14 +36,11 @@ def svm_train():
         x_train_tdm = content[L]
         x_test_tdm = content[U]
 
-        start = time.time()
         SVM.fit(x_train_tdm, label[L])
-        print(time.time() - start)
 
         predict_proba_test = SVM.predict_proba(x_test_tdm)
         predict = np.argmax(predict_proba_test, axis=1)
         max_proba = np.array([max(i) for i in predict_proba_test])
-        #split_group = int(max_proba.size * 0.2)
         if U.size < 10000:
             split_group=U.size
             max_index = np.argsort(max_proba)[-split_group:]
@@ -84,7 +79,6 @@ def predict():
     print(predict_test)
 
 #训练模型和保存模型
-#准确率： 0.9578121897955132
 #训练完可以将svm_train(x_train, x_test, y_train, y_test)注释掉节约预测时间
 svm_train()
 #预测方法示例
